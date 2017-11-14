39f05175aa663199c8e194cff07ae41604f4f593
Diego Soliz <zilosgodi@gmail.com>
2017-11-14 13:16:20 -0600

    Generated API Key Added Map module Added Map Activity and transition button.

--- app/src/main/java/com/example/bubbleup/MainActivity.java
+++ app/src/main/java/com/example/bubbleup/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.bubbleup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button my_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        my_button = (Button) findViewById(R.id.button2);
+
+        my_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
