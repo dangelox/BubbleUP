63140fe64d817d754a1ee112f44d9607952aca1a
Diego Soliz <zilosgodi@gmail.com>
2018-01-19 15:33:49 -0600

    -Fragment Views?

--- app/src/main/java/com/example/bubbleup/MainActivity.java
+++ app/src/main/java/com/example/bubbleup/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.bubbleup;
 
 import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -25,5 +27,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(myIntent);
             }
         });
+
+        /*
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        BlankFragment fragment = new BlankFragment();
+        fragmentTransaction.add(R.id.zone, fragment);
+        fragmentTransaction.commit();
+        */
     }
 }
--- app/src/main/java/com/example/bubbleup/MapsActivity.java
+++ app/src/main/java/com/example/bubbleup/MapsActivity.java
@@ -10,12 +10,12 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
-import android.view.View;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -57,11 +57,21 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMyLoca
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+
         mapFragment.getMapAsync(this);
+
+        //FragmentManager fragmentManager = getSupportFragmentManager();
+        //FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        //BlankFragment fragment = new BlankFragment();
+        //fragmentTransaction.add(R.id.map, fragment);
+        //fragmentTransaction.commit();
 
         mHandler = new Handler();//Create handler
     }
